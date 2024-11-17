0dc10a52c6eb4091e63a04e6950e6e9990f32778
peter.yang <nzai@163.com>
2024-11-17 15:35:16 +0800

    feat: first version

---
+++ data.js
@@ -0,0 +1,26 @@
+var data = {
+    "start_time": "2024/11/16 23:00:00",
+    "end_time": "2024/11/17 23:40:00",
+    "teams": [
+        {
+            "name": "Team A",
+            "score": 85
+        },
+        {
+            "name": "中国1队",
+            "score": 170
+        },
+        {
+            "name": "Team B",
+            "score": 100
+        },
+        {
+            "name": "Cagetest",
+            "score": 200
+        },
+        {
+            "name": "某某1队",
+            "score": 160
+        }
+    ]
+}
